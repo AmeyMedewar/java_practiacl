d862b0954ae0bc92990d4615c9be7899fd527660
Amey Medewar <2022bit153@sggs.ac.in>
2024-01-12 08:07:05 +0530

    added subtration function

--- calci.java
+++ calci.java
@@ -2,11 +2,14 @@ class calci{
 public static void main(String args []){
 int num1=10;
 int num2=20;
-int sum=add(num1,num2);
+int sum=add(num1,num2); //addition functionality
+int sub=minus(num1,num2); //subtraction functionality
 
 }
 static int add(int a,int b){
 return a+b;
 }
+static int minus(int a,int b){
+return a-b;
 }
-
+}
