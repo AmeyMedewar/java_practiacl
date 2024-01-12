6a2003d79c50fa1f2511a833ee80eb4103abfbb6
Amey Medewar <2022bit153@sggs.ac.in>
2024-01-12 08:53:53 +0530

    added multiplication function

--- calci.java
+++ calci.java
@@ -4,12 +4,15 @@ int num1=10;
 int num2=20;
 int sum=add(num1,num2); //addition functionality
 int sub=minus(num1,num2); //subtraction functionality
-
+int mul=multiply(num1,num2); //Multiplication functionality
 }
 static int add(int a,int b){
 return a+b;
 }
 static int minus(int a,int b){
 return a-b;
+}
+static int multiply(int a,int b){
+return a*b;
 }
 }
