b11f99ed25cf30170707e506fa3ebf214e70eb71
Amey Medewar <2022bit153@sggs.ac.in>
2024-01-12 18:45:41 +0530

    added square and cube

--- calci.java
+++ calci.java
@@ -5,9 +5,11 @@ int num2=20;
 int sum=add(num1,num2); //addition functionality
 int sub=minus(num1,num2); //subtraction functionality
 int mul=multiply(num1,num2); //Multiplication functionality
-float div=division(num1,num2); //division functionality
+float div=divide(num1,num2); //division functionality
 int mod=modulo(num1,num2); //modulus operation
 double expo=exponent(num1,num2); //exponential operation
+double sqr=square(num1); //square of num
+double cub=cube(num1); //cube of num
 
 }
 static int add(int a,int b){
@@ -26,10 +28,17 @@ return -1;
 else{
 return a/b;
 }
+}
 static int modulo(int a,int b){
 return a%b;
 }
 static double exponent(int a,int b){
 return a^b;
+}
+static double square(int a){
+return a*a;
+}
+static double cube(int a){
+return a*a*a;
 }
 }
